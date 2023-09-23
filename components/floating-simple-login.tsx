248c5075e2c88932dfa0a826093965db4c5d3ae1
aakindel <a.akindele01@gmail.com>
2023-09-23 14:30:00 -0600

    create floating simple login component

--- components/floating-simple-login.tsx
+++ components/floating-simple-login.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { AuthForm } from "./auth-form";
 
-export const SimpleLogin = () => {
+export const FloatingSimpleLogin = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center overflow-hidden">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div className="flex h-full w-full flex-col items-center justify-center overflow-hidden bg-neutral-100 px-4 dark:bg-neutral-900">
+      <div className="mx-auto flex w-full max-w-[414px] flex-col justify-center space-y-6 rounded-2xl bg-white px-8 pb-14 pt-14 shadow-md dark:bg-neutral-950">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Log in to Auth Pages
