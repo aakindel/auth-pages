f5a77aad4223ea9f13b6298a6c8e107b4218c1a3
aakindel <a.akindele01@gmail.com>
2023-09-26 15:19:30 -0600

    add radio group for selecting login components

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import {
   LoginLeftPanelSmall,
 } from "@/components/login-left-panel";
 import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { cn } from "@/utils";
 import {
   FloatingSimpleLogin,
@@ -59,6 +60,7 @@ const authComponents: AuthComponentType[] = [
 
 const Home: NextPage = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(true);
+  const [activeComponent, setActiveComponent] = useState(authComponents[0]);
 
   return (
     <div className="mx-auto min-h-screen w-full">
@@ -67,34 +69,49 @@ const Home: NextPage = () => {
         setIsPanelOpen={setIsPanelOpen}
       >
         <MainNav isPanelOpen={isPanelOpen} setIsPanelOpen={setIsPanelOpen} />
-        {authComponents[0].component}
+        {activeComponent.component}
         <RightPanelSidebar>
-          <div className="flex h-full w-[98%] flex-col gap-4 p-4">
-            {authComponents.map((authComponent, index) => {
-              return (
-                <React.Fragment key={index}>
-                  <div className="w-full">
-                    <div className="mb-2 flex items-center gap-2">
-                      <span className="block text-sm font-medium">
-                        {authComponent.title}
-                      </span>
+          <RadioGroup
+            value={activeComponent.id}
+            onValueChange={(value) =>
+              setActiveComponent(
+                authComponents.find((item) => item.id === value) ??
+                  authComponents[0]
+              )
+            }
+          >
+            <div className="flex h-full w-[98%] flex-col gap-4 p-4">
+              {authComponents.map((authComponent, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    <div className="w-full">
+                      <div className="mb-2 flex items-center gap-2">
+                        <span className="block text-sm font-medium">
+                          {authComponent.title}
+                        </span>
+                      </div>
+                      <div className="relative flex h-[200px] w-full items-center">
+                        <RadioGroupItem
+                          className="peer absolute right-4 top-4 z-50"
+                          value={authComponent.id}
+                          id={authComponent.id}
+                        />
+                        <Label
+                          htmlFor={authComponent.id}
+                          className={cn(
+                            "mx-auto flex h-[200px] w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 bg-white p-0 dark:bg-neutral-950",
+                            "hover:border-neutral-500 peer-aria-checked:border-neutral-900 dark:hover:border-neutral-400 dark:peer-aria-checked:border-neutral-50"
+                          )}
+                        >
+                          {authComponent.componentSmall}
+                        </Label>
+                      </div>
                     </div>
-                    <div className="relative flex h-[200px] w-full items-center">
-                      <Label
-                        htmlFor={authComponent.id}
-                        className={cn(
-                          "mx-auto flex h-[200px] w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 bg-white p-0 dark:bg-neutral-950",
-                          "hover:border-neutral-500 peer-aria-checked:border-neutral-900 dark:hover:border-neutral-400 dark:peer-aria-checked:border-neutral-50"
-                        )}
-                      >
-                        {authComponent.componentSmall}
-                      </Label>
-                    </div>
-                  </div>
-                </React.Fragment>
-              );
-            })}
-          </div>
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          </RadioGroup>
         </RightPanelSidebar>
       </RightPanelLayout>
     </div>
