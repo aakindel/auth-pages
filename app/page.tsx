dba4235bc6317df93be07a1898bf3841e7aea873
aakindel <a.akindele01@gmail.com>
2023-09-28 02:51:48 -0600

    lock login with left panel component if user is not logged in

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,15 @@ import {
 } from "@/components/login-left-panel";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/utils";
+import { LockIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import {
   FloatingSimpleLogin,
   FloatingSimpleLoginSmall,
@@ -23,6 +31,7 @@ type AuthComponentType = {
   title: string;
   component: React.ReactNode;
   componentSmall: React.ReactNode;
+  isLocked?: boolean;
 };
 
 const authComponents: AuthComponentType[] = [
@@ -55,10 +64,12 @@ const authComponents: AuthComponentType[] = [
       </div>
     ),
     componentSmall: <LoginLeftPanelSmall />,
+    isLocked: true,
   },
 ];
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
   const [isPanelOpen, setIsPanelOpen] = useState(true);
   const [activeComponent, setActiveComponent] = useState(authComponents[0]);
 
@@ -83,31 +94,46 @@ const Home: NextPage = () => {
             <div className="flex h-full w-[98%] flex-col gap-4 p-4">
               {authComponents.map((authComponent, index) => {
                 return (
-                  <React.Fragment key={index}>
-                    <div className="w-full">
-                      <div className="mb-2 flex items-center gap-2">
-                        <span className="block text-sm font-medium">
-                          {authComponent.title}
-                        </span>
-                      </div>
-                      <div className="relative flex h-[200px] w-full items-center">
-                        <RadioGroupItem
-                          className="peer absolute right-4 top-4 z-50"
-                          value={authComponent.id}
-                          id={authComponent.id}
-                        />
-                        <Label
-                          htmlFor={authComponent.id}
-                          className={cn(
-                            "mx-auto flex h-[200px] w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 bg-white p-0 dark:bg-neutral-950",
-                            "hover:border-neutral-500 peer-aria-checked:border-neutral-900 dark:hover:border-neutral-400 dark:peer-aria-checked:border-neutral-50"
+                  <TooltipProvider key={index} delayDuration={100}>
+                    <Tooltip>
+                      <div className="w-full">
+                        <div className="mb-2 flex items-center gap-2">
+                          <span className="block text-sm font-medium">
+                            {authComponent.title}
+                          </span>
+                          {authComponent?.isLocked && !session && (
+                            <div className="flex items-center justify-center rounded-full bg-neutral-200 p-[5px] dark:bg-neutral-700">
+                              <LockIcon className="h-[10px] w-[10px] shrink-0" />
+                            </div>
                           )}
-                        >
-                          {authComponent.componentSmall}
-                        </Label>
+                        </div>
+                        <TooltipTrigger asChild>
+                          <div className="relative flex h-[200px] w-full items-center">
+                            <RadioGroupItem
+                              className="peer absolute right-4 top-4"
+                              value={authComponent.id}
+                              id={authComponent.id}
+                              disabled={authComponent?.isLocked && !session}
+                            />
+                            <Label
+                              htmlFor={authComponent.id}
+                              className={cn(
+                                "mx-auto flex h-[200px] w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg border-2 bg-white p-0 dark:bg-neutral-950",
+                                "hover:border-neutral-500 peer-aria-checked:border-neutral-900 dark:hover:border-neutral-400 dark:peer-aria-checked:border-neutral-50"
+                              )}
+                            >
+                              {authComponent.componentSmall}
+                            </Label>
+                          </div>
+                        </TooltipTrigger>
                       </div>
-                    </div>
-                  </React.Fragment>
+                      {authComponent?.isLocked && !session && (
+                        <TooltipContent className="mb-0.5 ml-[245px] select-none px-2 py-1 text-xs shadow-sm duration-0 data-[state=closed]:duration-0">
+                          <p>Requires Login</p>
+                        </TooltipContent>
+                      )}
+                    </Tooltip>
+                  </TooltipProvider>
                 );
               })}
             </div>
